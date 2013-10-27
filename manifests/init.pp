c2b38d825c8e2b5663e279f2d64c2c4a016558fe
Kyle Anderson <kyle@xkyle.com>
2013-10-26 17:25:28 -0700

    Added more params

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,9 +13,16 @@
 # Kyle Anderson <kyle@xkyle.com>
 #
 class etcd (
-  $service_ensure = running,
-  $service_enable = true,
-  $package_ensure = installed,
+  $service_ensure  = running,
+  $service_enable  = true,
+  $package_ensure  = installed,
+  $use_ssl         = true,
+  $client_cert     = "/var/lib/puppet/ssl/certs/${::certname}.pem",
+  $client_key      = "/var/lib/puppet/ssl/private_keys/${::certname}.pem",
+  $node_name       = $::fqdn,
+  $user            = 'puppet',
+  $storage_dir     = '/var/lib/etcd/',
+  $cluster_members = [],
 ) inherits etcd::params {
 
   anchor { 'etcd::begin': } ->
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,14 @@
 class etcd::install {
 
   package { 'etcd':
-    ensure => $etcd::package_ensure,
+    ensure => $::etcd::package_ensure,
   }
   
+  file { $::etcd::storage_dir:
+    ensure => 'directory',
+    owner  => $::etcd::owner,
+    group  => 'root',
+    mode   => '0750',
+  }
+
 }
