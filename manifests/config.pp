5c5eebcaf3417bf4cc95f3daa433061ae88656ae
Mathew Finch <M.Finch@nib.com.au>
2014-02-28 16:27:47 +1100

    Removing the config file from puppet management since the rhel package includes a sysconfig file. this will need to be added to puppet in the future

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,12 +1,12 @@
 class etcd::config inherits etcd {
 
-  file { '/etc/etcd/etcd.conf':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0444',
-    content => template('etcd/etcd.conf.erb'),
-  }
+#  file { '/etc/etcd/etcd.conf':
+#    ensure  => present,
+#    owner   => 'root',
+#    group   => 'root',
+#    mode    => '0444',
+#    content => template('etcd/etcd.conf.erb'),
+#  }
 
   file { '/etc/init/etcd.conf':
     ensure  => $upstart,
